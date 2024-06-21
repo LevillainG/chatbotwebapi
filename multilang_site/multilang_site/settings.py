1ba4bdaa5bfbc7626e3d0e4b1690f20313bf1109
LevillainG <98540965+LevillainG@users.noreply.github.com>
2024-06-21 10:37:18 +0200

    Update settings.py CORS & CSRF changes

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -140,10 +140,11 @@ LOCALE_PATHS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://chatbotwebapi.onrender.com",
 ]
 
 #CSRF
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000', 'https://chatbotwebapi.onrender.com']
 
 
 HUGGINGFACE_API_TOKEN = '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'
